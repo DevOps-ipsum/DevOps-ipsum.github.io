f3a2ea92964cc710825bc7e6adaa0cd26e8a3b5b
Coleman Rollins <coleman@ten7.com>
2014-10-17 13:37:47 -0500

    JS works now

--- js/js.js
+++ js/js.js
@@ -1,16 +1,37 @@
-var opsArray = ['ops', 'drupal', 'backend', 'server', 'Apache', 'Solr', 'uptime', 'downtime', 'HostTracker', 'Passpack', 'Bash', 'Terminal', 'User', 'git', 'push', 'pull', 'commit', 'ipHouse', 'taxonomy', 'compile', 'DNS', '“launch”', 'deploy', 'A record', 'Settings file', 'htaccess', 'chmod', 'sudo', 'SQL', 'mysql', 'Brew', 'Node', 'curl', 'grep', 'log', 'rebase', 'Pair Networks', 'GoDaddy sucks', 'github', 'repository', 'PHP', 'query', 'merge', 'run cron', 't7-export', 't7-import', 'module updates', 't7 heartbeat', 'clone repo locally', 'preview environment', 'symbolic link', 'drush alias', 'Linux', 'Tika', 'JSON', 'Chef', 'Script', 'Master branch', 'Develop branch', 'core', 'host', 'API', 'rsync', 'brogrammer', 'Load testing', 'Load balancing', 'Mandrill', 'diff', 'pub key', 'private key', 'Varnish', 'cache', 'cron', 'nameserver', 'alias', 'WYSIWYG', 'ping', 'server side', 'post', 'head', 'get', 'top', 'ps aux', 'rbash', 'chown', 'more', 'htpasswd', 'cc all', 'App Neta', 'Slackbot', 'integration', 'mobile forward', 'responsiveness', 'multi platform functionality ', 'sideband demultiplexer']
 
 
-document.getElementById("ipsum").onclick = randString;
+// jQuery wrapper so there are no conflicts
+(function($) {
+	$(document).ready(function(){
+		// JS to run after the DOM is prepared.
+	var opsArray = ['ops', 'drupal', 'backend', 'server', 'Apache', 'Solr', 'uptime', 'downtime', 'HostTracker', 'Passpack', 'Bash', 'Terminal', 'User', 'git', 'push', 'pull', 'commit', 'ipHouse', 'taxonomy', 'compile', 'DNS', 'launch', 'deploy', 'A record', 'Settings file', 'htaccess', 'chmod', 'sudo', 'SQL', 'mysql', 'Brew', 'Node', 'curl', 'grep', 'log', 'rebase', 'Pair Networks', 'GoDaddy sucks', 'github', 'repository', 'PHP', 'query', 'merge', 'run cron', 't7-export', 't7-import', 'module updates', 't7 heartbeat', 'clone repo locally', 'preview environment', 'symbolic link', 'drush alias', 'Linux', 'Tika', 'JSON', 'Chef', 'Script', 'Master branch', 'Develop branch', 'core', 'host', 'API', 'rsync', 'brogrammer', 'Load testing', 'Load balancing', 'Mandrill', 'diff', 'pub key', 'private key', 'Varnish', 'cache', 'cron', 'nameserver', 'alias', 'WYSIWYG', 'ping', 'server side', 'post', 'head', 'get', 'top', 'ps aux', 'rbash', 'chown', 'more', 'htpasswd', 'cc all', 'App Neta', 'Slackbot', 'integration', 'mobile forward', 'responsiveness', 'multi platform functionality ', 'sideband demultiplexer']
 
-var randString = opsArray.randomElement();
 
-for (var i = 0; i < 250; i++) {
 
-	Array.prototype.randomElement = function () {
-    return this[Math.floor(Math.random() * this.length)]
-	}
+	$("#ipsum").click(function(){
+		$('#random-ipsum').hide();
 
-	return randString;
-}
+		var numPar = $('#paragraph').val();
+		var p = 0;
+		 while ( p < numPar) {
 
+					var i = 0;
+					while ( i < 100 ) {
+						opsString = opsArray[Math.floor(Math.random() * opsArray.length)];
+	
+						var newString = newString + ' ' + opsString;
+			    		// This block will be executed 100 times.
+			    		i++; // Increment i
+					}
+				   var newNewString = '<p>' + newString + '</p>';
+					$('#random-ipsum').append(newNewString);
+					newString = '';
+					p++;
+
+			 }
+			 $('#random-ipsum').fadeIn();
+			 
+		});	
+
+	});
+}(jQuery));
